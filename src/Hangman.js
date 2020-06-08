7ba0394f97e08c10c2bec34f671392a34c3d70ba
Michael Navarro <navarro.michael93@gmail.com>
2020-06-08 15:48:47 -0500

    Add key to components and end game conditional.

--- src/Hangman.js
+++ src/Hangman.js
@@ -46,6 +46,7 @@ class Hangman extends Component {
   generateButtons() {
     return "abcdefghijklmnopqrstuvwxyz".split("").map(ltr => (
       <button
+        key={ltr}
         value={ltr}
         onClick={this.handleGuess}
         disabled={this.state.guessed.has(ltr)}
@@ -61,8 +62,11 @@ class Hangman extends Component {
       <div className='Hangman'>
         <h1>Hangman</h1>
         <img src={this.props.images[this.state.nWrong]} />
+        <p>Guessed Wrong: {this.state.nWrong}</p>
         <p className='Hangman-word'>{this.guessedWord()}</p>
-        <p className='Hangman-btns'>{this.generateButtons()}</p>
+        <p className='Hangman-btns'>
+          {this.state.nWrong < this.props.maxWrong ? this.generateButtons() : `You lose: ${this.state.answer}`}
+        </p>
       </div>
     );
   }
